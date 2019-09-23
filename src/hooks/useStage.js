657dcd75b3b17551f32eb09b8b0f958c30b21a05
unknown <nbatre1@yahoo.com>
2019-09-23 18:59:55 -0500

    keep moving forward

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,5 +4,23 @@ import {createStage} from '../gameHelpers';
 
 export const useStage = (player,resetPlayer) => {
      const [stage,setStage] = useState(createStage());
+
+     useEffect(() =>{
+      
+       const updateStage = prevStage =>{
+
+          // first clear stage from previous render
+
+          const newStage = prev.Stage.map(row => row.map(cell =>(cell[i] === 'clear' ? [0,'clear'] : cell))
+          )
+          // then draw  new tetromino
+
+       };   
+
+       setStage(prev => updateStage(prev))
+
+
+     },[]);
+
      return[stage,setStage];
 }
